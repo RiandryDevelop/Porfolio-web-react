64c994c9b89e17c37c344834586b6c58a90cff7a
RiandryDevelop <riandrydevs@gmail.com>
2025-09-16 11:09:16 -0400

    (Update); contants.js
    - Fixing images path typos

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
   title: "Instagram Business Scraper API (Backend)",
   description:
     "An asynchronous API built with FastAPI that integrates advanced Instagram Business features: public profile scraping, direct messaging via the Facebook Graph API, business account linking, and usage control with PayPal-based plan verification. Includes secure user registration/login with JWT, database persistence with SQLAlchemy, and support for testing via Swagger UI or Postman. Ideal for marketing automation, CRM integration, or SaaS solutions that aim to scale client interactions on Instagram.",
-  image: "/images/InstagramScraperAPI_edit.png",
+  image: "/images/InstagramScraperAPI_edit.PNG",
   tags: ["Python", "FastAPI", "AsyncIO", "SQLAlchemy", "Instaloader", "PayPal API"],
   source: "https://riandrydevelop.github.io/instagram_scraper_api_public/",
   // visit: "https://github.com/RiandryDevelop/instagram_scraper_api_public",
@@ -12,7 +12,7 @@ export const projects = [
   { 
   title: "Sismo App (Full-Stack)",
   description: "Sismo App is a full-stack application that collects and delivers seismological data from around the world. The frontend is built with React, Vite, and TailwindCSS to provide a fast, modern, and scalable user interface. The backend is developed with Ruby on Rails, exposing a REST API to manage reports and comments. This solution combines an optimized frontend with a robust backend to deliver real-time seismic information.",
-  image: "/images/SismoApp_edit.png",
+  image: "/images/SismoApp_edit.PNG",
   tags: [
     "JavaScript", 
     "React", 
@@ -34,7 +34,7 @@ export const projects = [
   title: "Library App (Mobile)",
   description:
     "Library App is a cross-platform React Native mobile application that works as a personal online library. Users can securely sign up, sign in, and manage their virtual book collection. The backend, built with Node.js and Express, connects to Firebase for authentication and storage, offering full CRUD operations for book management.",
-  image: "/images/LibraryApp_edit.png",
+  image: "/images/LibraryApp_edit.PNG",
   tags: ["React Native", "Node.js", "Express", "Firebase"],
   source: "https://github.com/RiandryDevelop/LibraryAppUI",
   // visit: "https://github.com/RiandryDevelop/library_app",
@@ -45,7 +45,7 @@ export const projects = [
   title: "El juego de la ruleta (The Roulette Game)",
   description:
     "The Roulette Game is a web-based casino-style application where players can log in, manage their balance, and place bets on numbers, colors, or even/odd options. Built with Vue 3, TypeScript, and Pinia for state management, it features interactive components for spinning the roulette wheel, placing bets, and tracking results. The backend is powered by .NET and SQL Server, with Docker support for deployment.",
-  image: "/images/RouletteGame_edit.png",
+  image: "/images/RouletteGame_edit.PNG",
   tags: ["Vue 3", "TypeScript", "Pinia", ".NET", "SQL Server", "Docker", "GameDev"],
   source: "https://github.com/RiandryDevelop/Prueba-Tecnica-RiandryConnor-El-Juego-de-la-Ruleta",
   // visit: "https://riandrydevelop.github.io/Prueba-Tecnica-RiandryConnor-El-Juego-de-la-Ruleta/",
