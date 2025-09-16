ed5c6b872b04b96d03dd1f98b7277a5e1382a71f
RiandryDevelop <riandrydevs@gmail.com>
2025-09-16 16:05:32 -0400

    (Update): Public\ and Hero.js
    - Adding new CV into my portafolio
    - Removing unused images
    - Updating the CV src button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => (
         </SectionText>
         <Button>
           <a
-                        href="/images/RiandryConnorCV_v3.pdf"
+                        href="/images/RiandryConnorCV_EN.pdf"
             target="_blank"
             style={{ color: "#0f1624" }} 
             >
