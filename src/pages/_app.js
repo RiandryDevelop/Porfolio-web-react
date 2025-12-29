a888687d94a243d65a4b0b51d3fdb02b55ea3e79
RiandryDevelop <riandrydevs@gmail.com>
2025-12-29 17:15:17 -0400

    fix: correct Theme import path for production build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import Theme from "../styles/Theme";
 import { appWithTranslation } from "next-i18next";
 import { SearchProvider } from "../context/SearchContext";
+import Theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
